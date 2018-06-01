37f92d2552069f5387461b1e9ffc02420dd515f5
bilinma <ma_xiaobin612@126.com>
2018-06-01 16:38:08 +0800

    modify

--- Test/src/com/ucloudlink/bo/Student.java
+++ Test/src/com/ucloudlink/bo/Student.java
@@ -3,7 +3,7 @@ package com.ucloudlink.bo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Student {
+public class Student implements Comparable<Student>{
 	private int id;
 
 	private String name;
@@ -12,6 +12,26 @@ public class Student {
 
 	// 一个学生有多个电话号码
 	List<Tel> tels = new ArrayList<Tel>();
+	
+	
+	
+	public Student() {
+		super();
+	}
+
+	public Student(int id) {
+		super();
+		this.id = id;
+	}
+
+
+
+	public Student(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
 
 	public int getId() {
 		return id;
@@ -43,5 +63,16 @@ public class Student {
 
 	public void setTels(List<Tel> tels) {
 		this.tels = tels;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		if(this.id>o.getId()){
+			return 1;
+		}else if(this.id<o.getId()){
+			return -1;
+		}else{
+			return 0;
+		}
 	}
 }
--- Test/src/com/ucloudlink/test/TestTreeSet.java
+++ Test/src/com/ucloudlink/test/TestTreeSet.java
@@ -1,5 +1,6 @@
 package com.ucloudlink.test;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -26,13 +27,37 @@ public class TestTreeSet {
         ts.add("cba");
         System.out.println(ts);
         
-        
+        //对象比较器
         TreeSet<Student> ts2 = new TreeSet<Student>();
-        ts2.add(new Student());
-        ts2.add(new Student());
-        ts2.add(new Student());
-        ts2.add(new Student());
-        ts2.add(new Student());
-        System.out.println(ts2);
+        ts2.add(new Student(0));
+        ts2.add(new Student(1));
+        ts2.add(new Student(2));
+        ts2.add(new Student(3));
+        ts2.add(new Student(4));
+        for(Student s : ts2){
+        	System.out.println(s.getId());
+        }
+        //TreeSet 添加比较器
+        TreeSet<Student> ts3 = new TreeSet<Student>(new Comparator<Student>(){
+        	@Override
+            public int compare(Student s1, Student s2) {
+        		if(s1.getId()>s2.getId()){
+        			return -1;
+        		}else if(s1.getId()>s2.getId()){
+        			return 1;
+        		}else{
+        			return 0;
+        		}
+        		
+            }
+    	});
+        ts3.add(new Student(0));
+        ts3.add(new Student(1));
+        ts3.add(new Student(2));
+        ts3.add(new Student(3));
+        ts3.add(new Student(4));
+        for(Student s : ts3){
+        	System.out.println(s.getId());
+        }
 	}
 }
