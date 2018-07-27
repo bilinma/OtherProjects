aabc1f359e2f1a338518ff7e146ce0ed74ef2c74
bilinma <ma_xiaobin612@126.com>
2018-07-27 18:27:16 +0800

    add

---
+++ mongodb-test/src/com/ucloudlink/springmongo/service/user/impl/WbOrderServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ucloudlink.springmongo.service.user.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.ucloudlink.springmongo.model.WbOrder;
+import com.ucloudlink.springmongo.service.common.impl.CommonServiceImpl;
+import com.ucloudlink.springmongo.service.user.IWbOrderService;
+
+@Service
+public class WbOrderServiceImpl extends CommonServiceImpl<WbOrder, String> implements IWbOrderService{
+
+}
