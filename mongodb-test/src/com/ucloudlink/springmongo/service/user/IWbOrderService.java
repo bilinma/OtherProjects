619ac1dd53251618fa2400b641cb68b372f42dad
bilinma <ma_xiaobin612@126.com>
2018-07-27 18:27:16 +0800

    add

---
+++ mongodb-test/src/com/ucloudlink/springmongo/service/user/IWbOrderService.java
@@ -0,0 +1,8 @@
+package com.ucloudlink.springmongo.service.user;
+
+import com.ucloudlink.springmongo.model.WbOrder;
+import com.ucloudlink.springmongo.service.common.ICommonService;
+
+public interface IWbOrderService extends ICommonService<WbOrder, String> {
+
+}
---
+++ mongodb-test/src/com/ucloudlink/springmongo/test/Test.java
@@ -0,0 +1,41 @@
+package com.ucloudlink.springmongo.test;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ucloudlink.springmongo.common.dto.Condition;
+import com.ucloudlink.springmongo.common.dto.Formula;
+import com.ucloudlink.springmongo.model.WbOrder;
+import com.ucloudlink.springmongo.service.user.IWbOrderService;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("mongodb.xml");
+ 
+		long scopeDay = 6 * 30 *24 * 3600 * 1000L;
+		long currentTime = System.currentTimeMillis();
+		long createTime = currentTime - scopeDay;
+		System.out.println("createTime="+createTime);
+		IWbOrderService wbOrderService = (IWbOrderService) context.getBean("wbOrderServiceImpl");
+		Condition condition = new Condition();
+
+		condition.addOrItem("expireTime", currentTime, Formula.LE);
+		condition.addOrItem("deliveryTime", Long.valueOf(currentTime - 1*3600*1000),Formula.LE);
+		condition.addOrItem("startTime", currentTime, Formula.GE);
+		condition.addItem("tmlStatus", "NORMAL", Formula.EQ);
+		condition.addItem("status", "WAIT_RETURN", Formula.EQ);
+		condition.addItem("createTime", createTime, Formula.GE);
+		condition.setOrderby("createTime", true);
+		condition.setCurrentPage(1);
+		condition.setPageSize(10);
+		List<WbOrder> orderList = wbOrderService.findByConditionNew(condition);
+		for(int i=0 ;i<orderList.size();i++){
+			System.out.println(orderList.get(i));
+		}
+		
+	}
+
+}
