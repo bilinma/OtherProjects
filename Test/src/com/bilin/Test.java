0ecdd8202bcd876ead91528bc2b728c868236db4
bilinma <ma_xiaobin612@126.com>
2019-12-16 15:07:31 +0800

    modify

--- Test/src/com/bilin/Test.java
+++ Test/src/com/bilin/Test.java
@@ -11,9 +11,9 @@ public class Test {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String input1 = sc.nextLine();
+		String input1 = sc.next();
 		
-		String input2 = sc.nextLine();
+		String input2 = sc.next();
 		
 		boolean flag = false;
 		
--- Test/src/com/bilin/TestLinuxDir.java
+++ Test/src/com/bilin/TestLinuxDir.java
@@ -9,26 +9,25 @@ public class TestLinuxDir {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        Scanner sc1 = new Scanner(System.in);
         String basePath = "";
         for (int i=0;i<n;i++){
         	//获取输入命令
-        	String cdCmd = sc1.nextLine();
-        	if(cdCmd != null&&!"".equals(cdCmd)&&cdCmd.length()>2){
-        		String path = cdCmd.substring(2).trim();
-        		//判断是否返回上级目录
-        		if("..".equals(path)){
-        			basePath = basePath.substring(0, basePath.lastIndexOf("/"));
+        	String cdCmd = sc.next();
+        	String path = sc.next();
+        	//判断是否返回上级目录
+        	if("..".equals(path)){
+        		basePath = basePath.substring(0, basePath.lastIndexOf("/"));
+        	}else{
+        		if(path.charAt(0)=='/'){
+        			basePath = path;
         		}else{
-        			if(path.charAt(0)=='/'){
-        				basePath = path;
-        			}else{
-        				basePath = basePath+"/"+path;
-        			}
+        			basePath = basePath+"/"+path;
         		}
+        	}
+        	/*if(cdCmd != null&&!"".equals(cdCmd)&&cdCmd.length()>2){
         	}else{
         		System.out.println("命令非法！");
-        	}
+        	}*/
         }
         System.out.println(basePath);
 	}
